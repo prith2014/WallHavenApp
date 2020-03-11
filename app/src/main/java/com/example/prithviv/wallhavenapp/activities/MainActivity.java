5d8fa471abaa2085997de8c3dedd81a632c74498
prith2014 <prith2014@gmail.com>
2020-03-10 20:46:12 -0400

    Removed unnecessary TextViews in Main Activity.

--- app/src/main/java/com/example/prithviv/wallhavenapp/activities/MainActivity.java
+++ app/src/main/java/com/example/prithviv/wallhavenapp/activities/MainActivity.java
@@ -21,11 +21,10 @@ public class MainActivity extends AppCompatActivity
         implements  HomeFragment.OnFragmentInteractionListener,
                     SearchFragment.OnFragmentInteractionListener {
 
-    private TextView mTextMessage;
+    //private TextView mTextMessage;
     final Fragment fragmentHome = new HomeFragment();
     final Fragment fragmentSearch = new SearchFragment();
     final FragmentManager fm = getSupportFragmentManager();
-    ImageView testImage;
 
     Fragment active = fragmentHome;
 
@@ -36,17 +35,17 @@ public class MainActivity extends AppCompatActivity
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    //mTextMessage.setText(R.string.title_home);
                     fm.beginTransaction().hide(active).show(fragmentHome).commit();
                     active = fragmentHome;
                     return true;
                 case R.id.search_dashboard:
-                    mTextMessage.setText(R.string.title_search);
+                    //mTextMessage.setText(R.string.title_search);
                     fm.beginTransaction().hide(active).show(fragmentSearch).commit();
                     active = fragmentSearch;
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    //mTextMessage.setText(R.string.title_notifications);
                     return true;
             }
             return false;
@@ -59,34 +58,12 @@ public class MainActivity extends AppCompatActivity
 
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        mTextMessage = findViewById(R.id.message);
+        //mTextMessage = findViewById(R.id.message);
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
         //Fragments
         fm.beginTransaction().add(R.id.main_container, fragmentSearch, "fragSearch").hide(fragmentSearch).commit();
         fm.beginTransaction().add(R.id.main_container, fragmentHome, "fragHome").commit();
-        final TextView textView = findViewById(R.id.text);
-
-        // Request Queue
-        /*
-        RequestQueue queue = Volley.newRequestQueue(this);
-        String url = "https://wallhaven.cc/api/v1/w/2e99dg";
-
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                //textView.setText("Response is: "+ response);
-                Log.d("Response", response);
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                //textView.setText("That didn't work!");
-                Log.d("Error.Response", error.toString());
-            }
-        });
-        queue.add(stringRequest);
-        */
     }
 
     @Override
