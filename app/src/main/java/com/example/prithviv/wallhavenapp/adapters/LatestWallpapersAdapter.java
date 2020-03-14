496bbada1fc302b5c0a0933e0a251dc9d887a3d1
Prithvi Venkat <prith2014@gmail.com>
2020-03-13 22:45:28 -0400

    Added SelectedWallpaper Fragment.

--- app/src/main/java/com/example/prithviv/wallhavenapp/adapters/LatestWallpapersAdapter.java
+++ app/src/main/java/com/example/prithviv/wallhavenapp/adapters/LatestWallpapersAdapter.java
@@ -1,16 +1,20 @@
 package com.example.prithviv.wallhavenapp.adapters;
 
 import android.content.Context;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 
 import com.example.prithviv.wallhavenapp.R;
+import com.example.prithviv.wallhavenapp.fragments.SelectedWallpaperFragment;
 import com.example.prithviv.wallhavenapp.models.Wallpaper;
 import com.facebook.drawee.view.SimpleDraweeView;
 import com.facebook.imagepipeline.request.ImageRequest;
@@ -19,12 +23,14 @@ import com.facebook.imagepipeline.request.ImageRequestBuilder;
 import java.util.List;
 import java.util.Random;
 
+import static android.content.ContentValues.TAG;
+
 
 public class LatestWallpapersAdapter extends RecyclerView.Adapter<LatestWallpapersAdapter.ViewHolder> {
 
     private List<Wallpaper> mData;
     private LayoutInflater mInflater;
-    private AdapterView.OnItemClickListener mClickListener;
+    //private View.OnClickListener mClickListener;
 
     public LatestWallpapersAdapter(Context context, List<Wallpaper> data) {
         this.mInflater = LayoutInflater.from(context);
@@ -35,10 +41,13 @@ public class LatestWallpapersAdapter extends RecyclerView.Adapter<LatestWallpape
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
         View view = mInflater.inflate(R.layout.recyclerview_row_toplist, viewGroup, false);
-        return new ViewHolder(view);
+        final ViewHolder viewHolder = new ViewHolder(view);
+        view.setOnClickListener(viewHolder);
+
+        return viewHolder;
     }
 
-    // Binds the data to the TextView in each row
+    // Binds the data to the SimpleDraweeView in each row
     @Override
     public void onBindViewHolder(ViewHolder viewHolder, int position) {
         String urlThumbOriginal = mData.get(position).getThumbsOriginal();
@@ -46,6 +55,7 @@ public class LatestWallpapersAdapter extends RecyclerView.Adapter<LatestWallpape
                 ImageRequestBuilder.newBuilderWithSource(Uri.parse(urlThumbOriginal))
                         .build();
         viewHolder.mSimpleDraweeView.setImageRequest(imageRequest);
+
     }
 
     // Total number of rows
@@ -71,7 +81,21 @@ public class LatestWallpapersAdapter extends RecyclerView.Adapter<LatestWallpape
 
         @Override
         public void onClick(View view) {
-            //if (mClickListener != null) mClickListener.onItemClick(view, getAdapterPosition());
+            /*
+            if (mClickListener != null) {
+                Log.d(TAG, "position = " + this.getAdapterPosition());
+                //mClickListener.onItemClick(view, this.getAdapterPosition());
+
+            }
+            */
+
+            Log.d(TAG, "position = " + this.getAdapterPosition());
+            Wallpaper selectedWallpaper = mData.get(getAdapterPosition());
+            Log.d("Selected Wallpaper", selectedWallpaper.getURL());
+
+            Fragment selectedWallpaperFragment = new SelectedWallpaperFragment();
+            //FragmentManager fragmentManager = getFragmentManager();
+
         }
     }
 }
---
+++ app/src/main/java/com/example/prithviv/wallhavenapp/fragments/SelectedWallpaperFragment.java
@@ -0,0 +1,65 @@
+package com.example.prithviv.wallhavenapp.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.prithviv.wallhavenapp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link SelectedWallpaperFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class SelectedWallpaperFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public SelectedWallpaperFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment SelectedWallpaperFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static SelectedWallpaperFragment newInstance(String param1, String param2) {
+        SelectedWallpaperFragment fragment = new SelectedWallpaperFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_selected_wallpaper, container, false);
+    }
+}
