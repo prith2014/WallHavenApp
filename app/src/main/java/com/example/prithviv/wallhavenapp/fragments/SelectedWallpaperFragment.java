1dac28add5e624c03aaa350d873ee6bd320892dc
Prithvi Venkat <prith2014@gmail.com>
2020-05-09 15:40:39 -0400

    Added tags to SelectedWallpaper fragment. Modified colors for Light and Dark mode.

--- app/src/main/java/com/example/prithviv/wallhavenapp/fragments/SelectedWallpaperFragment.java
+++ app/src/main/java/com/example/prithviv/wallhavenapp/fragments/SelectedWallpaperFragment.java
@@ -18,6 +18,7 @@ import com.example.prithviv.wallhavenapp.ContextProvider;
 import com.example.prithviv.wallhavenapp.HttpRequest.RetrofitServer;
 import com.example.prithviv.wallhavenapp.R;
 import com.example.prithviv.wallhavenapp.models.Data;
+import com.example.prithviv.wallhavenapp.models.Tag;
 import com.example.prithviv.wallhavenapp.models.Wallpaper;
 import com.facebook.drawee.view.SimpleDraweeView;
 import com.facebook.imagepipeline.request.ImageRequest;
@@ -25,6 +26,7 @@ import com.facebook.imagepipeline.request.ImageRequestBuilder;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 
+import java.util.List;
 import java.util.Locale;
 
 import retrofit2.Call;
@@ -60,6 +62,7 @@ public class SelectedWallpaperFragment extends Fragment {
     private TextView textViewSize;
     private TextView textViewViews;
     private TextView textViewRating;
+    private TextView textViewTags;
 
     public SelectedWallpaperFragment(ContextProvider contextProvider) {
         // Required empty public constructor
@@ -120,6 +123,7 @@ public class SelectedWallpaperFragment extends Fragment {
         textViewSize = selectedWallpaperView.findViewById(R.id.text_view_size);
         textViewViews = selectedWallpaperView.findViewById(R.id.text_view_views);
         textViewRating = selectedWallpaperView.findViewById(R.id.text_view_purity);
+        textViewTags = selectedWallpaperView.findViewById(R.id.text_view_tags);
 
         // Inflate the layout for this fragment
         return selectedWallpaperView;
@@ -163,6 +167,19 @@ public class SelectedWallpaperFragment extends Fragment {
         textViewSize.setText(String.format(Locale.getDefault(), "%d", wallpaper.getFileSize()));
         textViewViews.setText(String.format(Locale.getDefault(), "%d", wallpaper.getViews()));
         textViewRating.setText(wallpaper.getPurity());
+
+        List<Tag> tags = wallpaper.getTags();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < tags.size(); i++) {
+            Tag tag = tags.get(i);
+            sb.append(tag.getName());
+
+            if (i < tags.size() - 1) {
+                sb.append(", ");
+            }
+        }
+
+        textViewTags.setText(sb.toString());
     }
 
     private void downloadWallpaper(String url) {
