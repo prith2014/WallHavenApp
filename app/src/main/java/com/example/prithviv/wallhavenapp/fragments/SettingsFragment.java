d59009383d2f00bc0c6bcbcf7c4356d6a959889f
prith2014 <prith2014@gmail.com>
2020-09-25 19:17:41 -0400

    Removed Resolution Filter Button from settings; Removed unused code;

--- app/src/main/java/com/example/prithviv/wallhavenapp/fragments/SettingsFragment.java
+++ app/src/main/java/com/example/prithviv/wallhavenapp/fragments/SettingsFragment.java
@@ -3,20 +3,12 @@ package com.example.prithviv.wallhavenapp.fragments;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
-import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
 import com.example.prithviv.wallhavenapp.R;
 
 
 public class SettingsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener {
-
-    private final String GENERAL_CATEGORY = "com.example.wallhavenapp.generalcategory";
-    private final String ANIME_CATEGORY = "com.example.wallhavenapp.animecategory";
-    private final String PEOPLE_CATEGORY = "com.example.wallhavenapp.peoplecategory";
-
-    //private SwitchPreference generalSwitchPreference;
-    //private SharedPreferences sharedPreferences;
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
@@ -38,9 +30,7 @@ public class SettingsFragment extends PreferenceFragmentCompat implements Shared
 
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-        if (key.equals(GENERAL_CATEGORY)) {
-            Preference pref = findPreference(key);
-        }
+
     }
 }
 
